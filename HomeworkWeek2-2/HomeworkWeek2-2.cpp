74af4947f4e456b4380a280baea962b772e0985d
Joseph Cuesta Acevedo <N1001723@my.ntu.ac.uk>
2022-01-24 22:19:13 +0000

    Completed homwork2

--- HomeworkWeek2-2/HomeworkWeek2-2.cpp
+++ HomeworkWeek2-2/HomeworkWeek2-2.cpp
@@ -1,8 +1,48 @@
 using namespace std;
 #include <iostream>
+#include <string>
 
+float floatValidation(string inputMessage)
+{
+    bool loopControl = true;
+    string rawValue;
+    while (loopControl)
+    {
+        cout << inputMessage << endl;
+        cin >> rawValue;
+        try
+        {
+            float convertedValue = stod(rawValue);
+            loopControl = false;
+            return convertedValue;
+        }
+        catch (const std::exception&)
+        {
+            cout << "Value " << rawValue << " couldn't be converted to Float, Try again" << endl;
+        }
+    }
+}
+void userConverter()
+{
+    double multiplier = 28.35;
+    double userInput = floatValidation("Enter the amount of ounces you want to convert: \n");
+    double grams = userInput * multiplier;
+    cout << userInput << " ounces is approximately " << grams << " grams" << endl;
+}
+
+void conversionTable()
+{
+    int ounces[10] = { 1,2,3,4,5,6,7,8,9,10 };
+    double multiplier = 28.35;
+    for (size_t z = 0; z < 10; z++)
+    {
+        cout << ounces[z] << " ounces is " << ounces[z] * multiplier << " grams" << endl;
+    }
+}
 int main()
 {
-    cout << "Hello World!\n";
+    userConverter();
+    conversionTable();
 }
+
 
